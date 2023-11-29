e02a9682cef1da5f929b8b01ebab2b68068d4085
ergdevops <honor6firmware@gmail.com>
2023-11-29 16:45:51 +0000

    PixelOS bringup

--- aosp_raphael.mk
+++ aosp_raphael.mk
@@ -16,7 +16,7 @@
 $(call inherit-product, device/xiaomi/raphael/device.mk)
 
 # Inherit some common DerpFest stuff.
-$(call inherit-product, vendor/derp/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 
 # Bootanimation Resolution
@@ -28,16 +28,13 @@ TARGET_INCLUDE_PIXEL_CHARGER := true
 # Disable/Enable Blur Support, default is false
 TARGET_ENABLE_BLUR := true
 
-# Derp
-DERP_BUILDTYPE := Official
-
 EXTRA_UDFPS_ANIMATIONS := true
 
 # Quick Tap Feature
 TARGET_SUPPORTS_QUICK_TAP := true
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := derp_raphael
+PRODUCT_NAME := aosp_raphael
 PRODUCT_DEVICE := raphael
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := MI 9T Pro
--- device.mk
+++ device.mk
@@ -62,7 +62,7 @@ PRODUCT_PACKAGES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-derp
+    $(LOCAL_PATH)/overlay-aosp
 
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal
